37c4e12816d0adf079f670b6084373ecbe989182
Patrick <alip@uni.coventry.ac.uk>
2019-07-04 02:41:18 +0100

    Automating program creation

--- Py_Write.py
+++ Py_Write.py
@@ -2,24 +2,83 @@ from JSON import JSON
 
 import subprocess
 
+#Create function
+#Create variables
+#Create ability to perform operation
+#Create return statement
 
-def test(file, text):
-    with open(file, 'a') as f:
-        f.write(text)
+class PyCreate():
+
+    reader = JSON()
+    
+    def create_function(self, name, param_name, operation):
+        indent = "   "
+        new_line = "\n"
+        line = self.reader.getData("py", "function", "code")
+        line = line.replace("<name>", name)
+        params = []
+        for p_name in param_name:
+            param = self.create_param(True, p_name)
+            params.append(param)
+        param = ''
+        for p in params:
+            param += (p + ", ")
+        line = line.replace("<param>", param)
+        line_two = self.create_body(operation, params)
+        line_three = self.create_return(operation)
+        text = line + new_line + indent + line_two + new_line + indent + line_three
+
+        self.write_line("test.py", text)
+        
+    def create_param(self, func_param, name, value = "[]"):
+        param = name
+        if not func_param:
+            param = name + " = " + value
+        return param
+            
+    def create_body(self, operation, variables):
+        # Suggestion - variables, values, operations, return
+        line = self.reader.getData("py", operation, "code")
+        for var in variables:
+           line.replace("<name" + str(var) + ">", var)
+        return line
+    
+    def create_return(self, operation):
+        line = self.reader.getData("py", operation, "return")
+        return line
+
+    def create_app_run(self, call, params):
+        indent = "   "
+        new_line = "\n"
+        line = self.reader.getData("py", "run")
+        line_two = call
+        for p in params:
+            line_two += (p + ",")
+        text = new_line + line + new_line + indent + line_two + ")"
+        self.write_line("test.py", text)
+            
+    def write_line(self, file, text):
+        with open(file, 'a') as f:
+            f.write(text)
         #f.write('print("Hello World")')
-def test_call():
-    test = subprocess.run("python test2.py",
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-    check=True,
-    text=True,
-    shell=True)
-
-    print(test.stdout)
+            
+    def call_py(self, file):
+        #**kwargs, args for input variables
+        test = subprocess.run("python " + file,
+                              stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                              check=True,
+                              text=True,
+                              shell=True)
+        #print(test.stdout)
+        return test.stdout
 
 if __name__ == '__main__':
-    indent = "   "
-    new_line = "\n"
-    test_call()
+    test = PyCreate()
+    test.create_function("add", ["num1", "num2"], "add")
+    test.create_app_run("add(", ["1", "2"])
+    #indent = "   "
+    #new_line = "\n"
+    #test_call()
     #reader = JSON()
     #line = reader.getData("py", "run")
     #test("test2.py", line + new_line)
---
+++ test.py
@@ -0,0 +1,5 @@
+def add(num1, num2, ):
+   op_add = <var1> + <var2>
+return op_add
+if __name__ == '__main__':
+   add(1,2,)
