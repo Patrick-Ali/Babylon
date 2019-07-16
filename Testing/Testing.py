5f4b04e39e81b9a4a6395368fd3a1e881064a398
Patrick <alip@uni.coventry.ac.uk>
2019-07-16 01:58:33 +0100

    Converting number words to numbers

--- Testing/Testing.py
+++ Testing/Testing.py
@@ -14,11 +14,12 @@ def multi(num1, num2):
     num3 = num1 * num2
     return num3
 
-def getNum(num, last_num, cur_num, extra, last):
+def getNum(num, last_num, cur_num, extra, last, next_num):
     '''Determine the numbers in the sentence'''
     data = js()
     nums = data.loadData("numbers")
     print("Extra: " + str(extra))
+    words = ["hundred","thousand","million","billion","trillion"]
     #print(num.lower())
     for element in nums:
         #print(element)
@@ -33,25 +34,42 @@ def getNum(num, last_num, cur_num, extra, last):
             #last = 0
             if element in multis:
                 numb = cur_num
+                multi_now = False
                 if numb == 0:
                     numb = last
                     cur_num = last
                     print("Last: " + str(last))
                 if element == "00":
                     numb = multi(cur_num, 100)
+                    multi_now = True
                 elif element == "000":
                     numb = multi(cur_num, 1000)
+                    multi_now = True
                 elif element == "000000":
                     numb = multi(cur_num, 1000000)
+                    multi_now = True
                 elif element == "000000000":
                     numb = multi(cur_num, 1000000000)
+                    multi_now = True
                 elif element == "000000000000":
                     numb = multi(cur_num, 1000000000000)
+                    multi_now = True
                 last_num = False
-                if extra != 0:
-                    print("Extra ")
-                    last = numb
-                    numb += extra
+                last = numb
+                if extra != 0: #and jump == False:
+                    print("Hello " + str(numb))
+                    #last = numb
+                    #if jump == False:
+                    print("Num " + num)
+                    print("Next Word " + next_word)
+                    if extra == cur_num:
+                        numb = numb
+                    elif (num in words and next_word not in words or num not in words and next_word not in words or num not in words and next_word in words): #and extra != cur_num: #: #and multi_now == False
+                        print("Cur num " + str(cur_num))
+                        numb = add(numb, extra)
+                    #elif multi_now == False:
+                    else:
+                        numb = extra
                 return (numb, 1, last)
             numb = int(element)
             print("Before " + str(num))
@@ -84,14 +102,34 @@ def regexe(text):
 
 if __name__ == "__main__":
     hold = ta()
-    temp = hold.tokenize_sentence("I want a program to add one million two hundred thousand thirty thousand four thousand five hundred sixty seven numbers togther.")
+    #Track sequence
+    #1,234,567,890
+    #1,234,567,890
+    #1,234,567,891,234
+    #temp = hold.tokenize_sentence("I want a program to add one million two hundred thousand thirty thousand four thousand five hundred sixty seven numbers togther.")
+    temp = hold.tokenize_sentence("I want a program to add one trillion trillion trillion two hundred billion thirty four billion five hundred million sixty seven million eight hundred thousand ninety one thousand two hundred thirty four numbers togther.")
+    #temp = hold.tokenize_sentence("I want a program to add one hundred thousand three numbers togther.")
+    #temp = hold.tokenize_sentence("I want a program to add one trillion")
     last_num = False
     cur_num = 0
     extra = False
     last = 0
+    next_word = ""
+    words = ["hundred","thousand","million","billion","trillion"]
+    jump = False
+    count = 0
     for word in temp:
-        num = getNum(word,last_num,cur_num, extra, last)
+        if count+1 < len(temp):
+            next_word = temp[count+1]
+        else:
+            next_word = ""
+        #if word in words and next_word in words:
+            #jump = True
+        #elif next_word not in words:
+            #jump = False
+        num = getNum(word,last_num,cur_num, extra, last, next_word)
         if num != "Unkown Number":
+            #last_word = word
             last_num = True
             extra_true = num[1]
             if extra_true == 3:
@@ -108,9 +146,11 @@ if __name__ == "__main__":
                 cur_num = num[0]
             
             print(num)
+            count += 1
             continue
         elif num == "Unkown Number":
             last_num = False
             cur_num = 0
+            count += 1
             
     print(regexe("I want a program to add 12345 x numbers togther."))
